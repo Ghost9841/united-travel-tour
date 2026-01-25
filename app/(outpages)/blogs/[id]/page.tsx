db097f11b11fe2ac0fb4034b541a0752753d5427
Ghost9841 <asubedi778@gmail.com>
2026-01-25 14:36:55 +0545

    Work on Travel Now

---
+++ app/(outpages)/blogs/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogsDetailPage = () => {
+  return (
+    <div>BlogsDetailPage</div>
+  )
+}
+
+export default BlogsDetailPage
---
+++ app/(outpages)/travels/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TravelDetailPage = () => {
+    // uselocalsearchparams here and then the folder has [id] 
+    // so it will use it give it to ai it will do it
+  return (
+    <div>TravelDetailPage</div>
+  )
+}
+
+export default TravelDetailPage
