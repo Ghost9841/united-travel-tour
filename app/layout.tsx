cd2212a3f42533e7949d1bd72b8069dfa711bacd
Ghost9841 <asubedi778@gmail.com>
2026-02-01 21:06:04 +0545

    Top Thin Bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import TopThinNavbar from "@/components/landingpage/TopThinNavbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <TopThinNavbar/>
         {children}
         <Script
           id="tawk-to"
