dc44d1a026b04e458315319d22ef508c9b3a52da
Ghost9841 <asubedi778@gmail.com>
2026-02-24 18:46:04 +0545

    lets hope this works

--- app/(outpages)/aboutus/AUHeroSection.tsx
+++ app/(outpages)/aboutus/AUHeroSection.tsx
@@ -3,7 +3,7 @@ import { Star, Zap } from 'lucide-react';
 
 const HeroSection = () => {
   return (
-    <div className="relative h-[70vh] bg-gradient-to-r from-orange-200 via-orange-200 to-red-200 overflow-hidden ">
+    <div className="relative h-[70vh] overflow-hidden ">
       <div className="absolute inset-0 mt-14">
         <img 
           src="https://images.unsplash.com/photo-1488646953014-85cb44e25828?w=1920&h=1080&fit=crop"
