9b94853e506b7ede5ec7c368d55f933a840839e2
Ghost Aakash <88389544+Ghost9841@users.noreply.github.com>
2026-03-07 13:14:03 +0000

    aaile ko lagi change

---
+++ app/api/blogs/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import destinationData from "@/app/data/destinations.json";
+import Destination from "../destinations/types";
+
+
+export async function GET() {
+  const destinations : Destination[] = destinationData.destinations;
+  return NextResponse.json({
+    success : true,
+    data : destinations
+  })
+}
