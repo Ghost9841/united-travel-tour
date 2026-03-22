954295f44a997f9ed8cd3f2ac690a549e654bfc4
Ghost Aakash <88389544+Ghost9841@users.noreply.github.com>
2026-03-22 12:04:39 +0000

    send now

--- app/(outpages)/booknow/page.tsx
+++ app/(outpages)/booknow/page.tsx
@@ -441,7 +441,7 @@ export default function ModernBookNow() {
                           </span>
                         ) : (
                           <span className="flex items-center justify-center gap-3">
-                            ✈️ Get Your Custom Quote Now
+                             Send Now
                             <Clock className="w-5 h-5" />
                           </span>
                         )}
